b943c51bc17327e28fb7ef7b84cfcf1eac053723
jboegeholz <boegeholz.joern@gmail.com>
2019-10-03 12:51:05 +0200

    rework test case

--- test/test_pip.py
+++ test/test_pip.py
@@ -1,17 +1,18 @@
 import unittest
 from pip._internal.utils.misc import get_installed_distributions
 
+
 class PipTest(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.package_name = "flask"
 
     def test_pip_install(self):
         from pip._internal import main as pipmain
-        pipmain(['install', "flask"])
+        pipmain(['install', self.package_name])
 
     def test_pip_uninstall(self):
         from pip._internal import main as pipmain
-        pipmain(['uninstall', "--yes", "flask"])
+        pipmain(['uninstall', "--yes", self.package_name])
 
     def test_freeze(self):
         with open("requirements.txt", "w") as f:
@@ -19,8 +20,6 @@ class PipTest(unittest.TestCase):
                 req = dist.as_requirement()
                 f.write(str(req) + "\n")
 
-    def tearDown(self) -> None:
-        pass
 
 
 if __name__ == '__main__':
